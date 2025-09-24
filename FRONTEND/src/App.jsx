ec7573e78dfc4f9d4a3a6768ad19bcf65b2d521e
Abhijith545 <abhijithnani826@gmail.com>
2025-09-24 16:19:58 +0530

    commiting the skeleton of the project

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (<>
+   
+  </>)
+}
+
+export default App
