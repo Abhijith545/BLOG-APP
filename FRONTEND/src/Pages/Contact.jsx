5914de996df350acca2645ddc974acbe2bcf2bf8
Abhijith545 <abhijithnani826@gmail.com>
2025-09-25 21:16:18 +0530

    Extra Pages for Navbar

---
+++ FRONTEND/src/Pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Base from '../Components/Base'
+
+const Contact = () => {
+  return (
+    <Base>
+        <h1>Contact page</h1>
+        <p>Please contact</p>
+    </Base>
+  )
+}
+
+export default Contact
